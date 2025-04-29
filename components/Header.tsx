4a5d1bc84ce3efbe5ee5d9f7cf74f735c9179f0d
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-04-29 22:19:42 +0200

    .

--- components/Header.tsx
+++ components/Header.tsx
@@ -156,7 +156,7 @@ export default function Header() {
           : "bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 shadow-md"
       }  fixed top-0 left-0 right-0 z-[50] transition-colors`}
     >
-      <nav className="flex items-center justify-between px-2 md:px-8 py-3 max-w-[80rem] w-full text-primary font-bold">
+      <nav className="flex items-center justify-between px-4 md:px-8 py-3 max-w-[80rem] w-full text-primary font-bold">
         <Link href="/" className="">
           <Image
             src={Logo}
